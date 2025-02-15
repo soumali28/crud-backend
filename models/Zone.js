93f34a7355831cff064e62879765de886bb79a61
Debdeep1 <deb.baner.1@gmail.com>
2025-02-15 14:08:22 +0530

    zone schema altered

--- models/Zone.js
+++ models/Zone.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
+
 const zoneSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
+        required: true,
     },
-    zonalNumber:{
+    zonalNumber: {
         type: Number,
-        required:false,
+        required: false,
     },
-    zonalLandmark:{
-        type:String,
-        required:false,
+    zonalLandmark: {  // Changed to an array of strings
+        type: [String],  
+        required: false,
     }
 });
 
